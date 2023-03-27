81629ce10da244be1a1a168fd8631e91ac96db82
Qile <qile0317@gmail.com>
2023-03-28 01:20:24 +0200

    continuation of painful namechanging

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(scballpack)
+library(APackOfTheClones)
 
-test_check("scballpack")
+test_check("APackOfTheClones")
