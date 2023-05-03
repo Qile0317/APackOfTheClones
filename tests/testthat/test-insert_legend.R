54b79f51df299724d170adf2567985c88cf26bab
Qile <qile0317@gmail.com>
2023-05-03 19:41:55 +0200

    fixed test and namespaxe

--- tests/testthat/test-insert_legend.R
+++ tests/testthat/test-insert_legend.R
@@ -6,4 +6,10 @@ test_that("insert_legend() works, assuming clonal_expansion_plot() works", {
     clonal_expansion_plot(
       test_pbmc, test_tcr, verbose = F, clone_scale_factor = 0.5, retain_axis_scales = F,
       add_size_legend = T, legend_sizes = c(1, 5, 10), legend_buffer = 0))
+  
+  expect_doppelganger(
+    "main_plot_with_smaller_custom_legend",
+    clonal_expansion_plot(
+      test_pbmc, test_tcr, verbose = F, clone_scale_factor = 0.5, retain_axis_scales = F,
+      add_size_legend = T, legend_sizes = c(1, 3), legend_buffer = 0.5))
 })
