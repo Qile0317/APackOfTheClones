04b130b877412d68487b3551f668e8cccb3aaa10
Qile <qile0317@gmail.com>
2023-07-16 21:33:07 +0200

    more cleanup

--- R/parameter_estimators.R
+++
@@ -1,5 +0,0 @@
-# script to estimate some okay-looking parameters for the plot
-
-estimate_clone_scale_factor <- function() {
-  
-}
--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("\nThank you for using APackOfTheClones! \n")
     packageStartupMessage("Please read the vignette found at https://qile0317.github.io/APackOfTheClones/articles/web_only/Clonal_expansion_plotting.html")
-    packageStartupMessage("if you use this package in your paper, please cite this package with the citation() function\n")
+    packageStartupMessage("\nif you use this package in your paper, please cite this package with the citation() function\n")
     #packageStartupMessage(paste(
     #  "R package Version", 
     #  packageVersion("APackOfTheClones"),
