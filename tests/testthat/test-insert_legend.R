4d8cc0b91937c87ccffc26a64076e3ae51d43583
Qile0317 <qile0317@gmail.com>
2025-08-02 15:50:32 -0700

    add correct legend tests

--- tests/testthat/test-insert_legend.R
+++ tests/testthat/test-insert_legend.R
@@ -1,20 +1,39 @@
 test_that("overlayLegend() and removeLegend() works", {
     data("combined_pbmc")
 
-    expect_equal(
-        vizAPOTC(combined_pbmc, verbose = FALSE),
-        vizAPOTC(combined_pbmc, verbose = FALSE) %>% overlayLegend()
+    expect_doppelganger(
+        "overlayLegend plot",
+        vizAPOTC(combined_pbmc, verbose = FALSE)
     )
 
-    expect_equal(
-        vizAPOTC(combined_pbmc, add_size_legend = FALSE, verbose = FALSE),
+    expect_doppelganger(
+        "overlayLegend plot",
+        vizAPOTC(combined_pbmc,  add_size_legend = FALSE, verbose = FALSE) %>%
+            overlayLegend()
+    )
+
+    expect_doppelganger(
+        "removeLegend plot",
+        vizAPOTC(combined_pbmc, add_size_legend = FALSE, verbose = FALSE)
+    )
+
+    expect_doppelganger(
+        "removeLegend plot",
         vizAPOTC(combined_pbmc, verbose = FALSE) %>% removeLegend()
     )
 
-    expect_equal(
-        vizAPOTC(combined_pbmc, verbose = FALSE),
+    expect_doppelganger(
+        "overlayLegend plot",
         vizAPOTC(combined_pbmc, verbose = FALSE) %>%
             removeLegend() %>%
             overlayLegend()
+    )
+
+    expect_doppelganger(
+        "removeLegend plot",
+        vizAPOTC(combined_pbmc, verbose = FALSE) %>%
+            removeLegend() %>%
+            overlayLegend() %>%
+            removeLegend()
     )
 })
